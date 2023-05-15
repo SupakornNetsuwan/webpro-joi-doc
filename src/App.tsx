8d21c50e1e9f76e648eb62ac100f78888e2d5a0e
supakornnetsuwan <earth_killerdark@hotmail.com>
2023-05-15 12:21:24 +0700

    fix : color

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="prose max-w-[60em] mx-auto my-24 prose-slate bg-slate-50 p-12 prose-code:bg-slate-200 prose-code:rounded-lg prose-code:p-1 prose-h1:text-blue-600 prose-h2:text-blue-500:">
+    <div className="prose max-w-[60em] mx-auto my-24 prose-slate bg-slate-50 p-12 prose-code:bg-slate-200 prose-code:rounded-lg prose-code:p-1 prose-h1:text-blue-600 prose-h2:text-blue-500 prose-h3:text-blue-400">
       <ReactMarkdown children={markdown} />
     </div>
   );
